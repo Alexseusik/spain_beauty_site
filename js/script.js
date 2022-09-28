01521d4adae2db38368e932ebb1847f4744a3cb1
Oleksii Hnybida <alexgnibeda03@gmail.com>
2022-09-29 00:53:09 +0300

    starting final updating v4.2
    starting mobile version

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let burgerbtn = document.querySelector("#burgerbtn");
+let burgermenu = document.querySelector("#burgermenu");
+
+function toggleBurger() {
+    burgerbtn.classList.toggle("active");
+    burgermenu.classList.toggle("active");
+}
