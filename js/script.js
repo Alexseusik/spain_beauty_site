09f7ac1889326b29fd2a8470abe94a63b2ededad
alexseyhnibida <alexgnibeda03@gmail.com>
2023-02-04 15:12:16 +0200

    change video + styling

--- js/script.js
+++ js/script.js
@@ -5,3 +5,26 @@ function toggleBurger() {
     burgerbtn.classList.toggle("active");
     burgermenu.classList.toggle("active");
 }
+
+// $(function () {
+//     // Header
+//     let header = $("#header");
+//     let main = $("#main");
+//     let mainH = main.innerHeight() - 20;
+//     let scrollPos = $(window).scrollTop();
+//     checkScroll(scrollPos,mainH);
+//
+//     $(window).on("scroll resize", function(){
+//         let mainH = main.innerHeight() - 20;
+//         scrollPos=$(this).scrollTop();
+//         checkScroll(scrollPos,mainH);
+//     });
+//
+//     function checkScroll(scrollPos,mainH) {
+//         if (scrollPos> mainH) {
+//             header.addClass("fixed");
+//         } else {
+//             header.removeClass("fixed");
+//         }
+//     }
+// })
