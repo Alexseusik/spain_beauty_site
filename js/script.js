766c1f6faddabc441da95919c192cb05c315192a
alexseyhnibida <alexgnibeda03@gmail.com>
2023-02-14 13:45:12 +0200

    full adaptive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 let burgerbtn = document.querySelector("#burgerbtn");
 let burgermenu = document.querySelector("#burgermenu");
-
 function toggleBurger() {
     burgerbtn.classList.toggle("active");
     burgermenu.classList.toggle("active");
