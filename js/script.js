ab18d0f66cadc57b36600ce314fae2bdbe308cca
alexseyhnibida <alexgnibeda03@gmail.com>
2023-04-20 14:55:15 +0300

    adding banner + adaptive banner

--- js/script.js
+++ js/script.js
@@ -2,37 +2,21 @@ let burgerbtn = document.querySelector("#burgerbtn");
 let burgermenu = document.querySelector("#burgermenu");
 
 let banner = document.querySelector("#banner");
-let bannerconntent = document.querySelector("#bannercontent");
-let bannerclosed = document.querySelector("#bannerclosed");
+let bannerinfo = document.querySelector("#bannerclosed");
+
+let courseinfo = document.querySelector("#courseInfo");
+let closeinfo = document.querySelector("#closeCourseInfo");
 
 function toggleBurger() {
     burgerbtn.classList.toggle("active");
     burgermenu.classList.toggle("active");
 }
-function closeAdBanner() {
-    // bannerconntent.classList.remove("animate__animated",
-    //     "animate__slideInUp");
-    bannerconntent.classList.add(/*"animate__animated",
-        "animate__slideOutDown",*/
-        "closed");
-    banner.classList.add("closed");
-    // bannerclosed.classList.remove("animate__slideOutDown");
-    bannerclosed.classList.add("active",
-        /*"animate__animated",
-        "animate__slideInUp"*/);
 
-}
-function openAdBanner() {
-    bannerconntent.classList.remove(/*"animate__animated",
-        "animate__slideOutDown",*/ "closed");
-    // bannerconntent.classList.add("animate__animated",
-    //     "animate__slideInUp");
-    banner.classList.remove("closed");
-    bannerclosed.classList.remove("active",
-        /*"animate__slideInUp"*/);
-    // bannerclosed.classList.add("animate__slideOutDown");
+function changeAdBanner() {
+    banner.classList.toggle("closed");
+    bannerinfo.classList.toggle("active");
 }
 
-function initMap() {
-
+function changeAdditionalBanner() {
+    courseinfo.classList.toggle("active");
 }
